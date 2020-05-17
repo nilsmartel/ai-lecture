c9b4a355c551df6038e60d3fdf589e699875d5d6
Nils Martel <nils.martel@graphmasters.net>
2020-05-17 16:36:18 +0200

    Refine solution

--- backtracking.py
+++ backtracking.py
@@ -2,20 +2,23 @@
 
 
 def backtrack(variables, domain, constraints, setup={}):
+    # Solution found
+    if len(variables) == 0:
+        return setup
+
     # create clones so we won't mutate call arguments
     setup = dict(setup)
-    variables = list(variables)
     # Chose a variable and remove it from the remaing variables
-    var = variables.pop()
+    var = variables[0]
+    variables = variables[1:]
+
+
     # set variable chosen at this step to all possilbe values
     # in domain
     for d in domain:
         setup[var] = d
         # all constraints are satisfied
         if all([c(setup) for c in constraints]):
-            # Solution found
-            if len(variables) == 0:
-                return setup
 
             result = backtrack(variables, domain, constraints, setup)
             # No solutions found on this way
@@ -30,16 +33,17 @@ def backtrack(variables, domain, constraints, setup={}):
 def example():
     variables = ['a', 'b', 'c']
     domain = [1, 2, 3]
-    # Only returns true, if all keys
+    # Only returns false, if all keys
     # can be found inside the dict
 
     def req(d, keys):
         m = [key in d for key in keys]
-        return all(m)
+        return not all(m)
+
     constraints = [
-        lambda d: req(d, ['a', 'b']) and d['a'] > d['b'],
-        lambda d: req(d, ['c', 'b']) and d['b'] != d['c'],
-        lambda d: req(d, ['a', 'c']) and d['a'] != d['c'],
+        lambda d: req(d, ['a', 'b']) or d['a'] > d['b'],
+        lambda d: req(d, ['c', 'b']) or d['b'] != d['c'],
+        lambda d: req(d, ['a', 'c']) or d['a'] != d['c'],
     ]
     backtrack(variables, domain, constraints)
 
